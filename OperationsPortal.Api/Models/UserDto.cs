2ff702edc9c5b6c706c703dc11ba1fea52418d28
Tati_ <Tati_>
2026-07-30 15:58:01 -0300

    feat: add Users controller (safe dto mapping)

---
+++ OperationsPortal.Api/Models/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationsPortal.Api.Models
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ OperationsPortal.Data/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using OperationsPortal.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OperationsPortal.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly OperationsPortalDbEntities1 _context;
+
+        public UserRepository()
+        {
+            _context = new OperationsPortalDbEntities1();
+            _context.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(x => x.UserId == id);
+        }
+    }
+}                           
